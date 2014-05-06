22eca8cac81fb5133b4719040e3c34966d5c17a1
Ole Morten <ole.morten.haaland@nordicsemi.no>
2014-05-06 15:49:19 +0200

    Create unique hash for attachments, if not exist

--- askbot/models/attachment.py
+++ askbot/models/attachment.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from django.db import models
+import os
+from binascii import hexlify
+
+def _create_hash():
+    return hexlify(os.urandom(16))
 
 class Attachment(models.Model):
-    filehash = models.CharField(max_length=200, unique=True)
+    filehash = models.CharField(max_length=200, default=_create_hash, unique=True)
     filename = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        tries = 0
+        while tries < 5:
+            try:
+                super(Attachment, self).save(*args, **kwargs)
+                break
+            except IntegrityError, e:
+                # hash already used
+                self.filehash = _create_hash()
+        else:
+            raise IntegrityError("Could not make a hash for attachment")
 
     class Meta:
         app_label = 'askbot'
