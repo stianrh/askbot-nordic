29ed8c80cdfd344dcd7c48a848065000d46ce553
Ole Morten <ole.morten.haaland@nordicsemi.no>
2014-05-07 12:08:09 +0200

    Use correct path in attachment view

--- askbot/views/attachment.py
+++ askbot/views/attachment.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 from django.http import HttpResponse
 from askbot.models.attachment import Attachment
 from django.conf import settings
@@ -7,7 +8,7 @@ from django.shortcuts import get_object_or_404
 def get_attachment(request, filehash):
     a = get_object_or_404(Attachment, filehash=filehash)
 
-    abs_path = "%s/%s" % (settings.PROJECT_ROOT, a.filename)
+    abs_path = os.path.join(settings.MEDIA_ROOT, a.filehash)
     
     response = HttpResponse() # 200 OK
     del response['content-type'] # We'll let the web server guess this.
