fcf8cb5116f54ab57be61b5c7bc4495e3eb99206
Ole Morten <ole.morten.haaland@nordicsemi.no>
2014-05-21 12:35:18 +0200

    Ignore case when converting bbcode

--- askbot/importers/easydiscuss/bbcode2markdown.py
+++ askbot/importers/easydiscuss/bbcode2markdown.py
@@ -4,32 +4,32 @@ import re
 
 def convert(bbcode):
     markdown = bbcode
-    markdown = re.sub(r'\[b\](.+?)\[/b\]', lambda x: r'**%s**' % x.group(1).strip(), markdown)
-    markdown = re.sub(r'\[i\](.+?)\[/i\]', lambda x: r'*%s*' % x.group(1).strip(), markdown)
-    markdown = re.sub(r'\[u\](.+?)\[/u\]', lambda x: r'<u>%s</u>' % x.group(1).strip(), markdown)
-    markdown = re.sub(r'\[s\](.+?)\[/s\]', lambda x: r'<del>%s</del>' % x.group(1).strip(), markdown)
-    markdown = re.sub(r'\[url\](.+?)\[/url\]', lambda x: r'[%s]' % x.group(1).strip(), markdown)
-    markdown = re.sub(r'\[url=?(.*?)\](.+?)\[/url\]', r'[\g<2>](\g<1>)', markdown, flags=re.DOTALL)
-    markdown = re.sub(r'\[img\](.+?)\[/img\]', r'![\g<1>](\g<1>)\n', markdown)
+    markdown = re.sub(r'\[b\](.+?)\[/b\]', lambda x: r'**%s**' % x.group(1).strip(), markdown, flags=re.IGNORECASE)
+    markdown = re.sub(r'\[i\](.+?)\[/i\]', lambda x: r'*%s*' % x.group(1).strip(), markdown, flags=re.IGNORECASE)
+    markdown = re.sub(r'\[u\](.+?)\[/u\]', lambda x: r'<u>%s</u>' % x.group(1).strip(), markdown, flags=re.IGNORECASE)
+    markdown = re.sub(r'\[s\](.+?)\[/s\]', lambda x: r'<del>%s</del>' % x.group(1).strip(), markdown, flags=re.IGNORECASE)
+    markdown = re.sub(r'\[url\](.+?)\[/url\]', lambda x: r'[%s]' % x.group(1).strip(), markdown, flags=re.IGNORECASE)
+    markdown = re.sub(r'\[url=?(.*?)\](.+?)\[/url\]', r'[\g<2>](\g<1>)', markdown, flags=re.DOTALL | re.IGNORECASE)
+    markdown = re.sub(r'\[img\](.+?)\[/img\]', r'![\g<1>](\g<1>)\n', markdown, flags=re.IGNORECASE)
 
-    markdown = re.sub(r'\[quote.*?\](.+?)\[/quote\]', r'> \g<1>\n', markdown)
-    bb_codes = re.findall(r'\[quote.*?\].*?\[/quote\]', markdown, flags=re.DOTALL)
+    markdown = re.sub(r'\[quote.*?\](.+?)\[/quote\]', r'> \g<1>\n', markdown, flags=re.IGNORECASE)
+    bb_codes = re.findall(r'\[quote.*?\].*?\[/quote\]', markdown, flags=re.DOTALL | re.IGNORECASE)
     for bb_code in bb_codes:
-        cleaned = re.sub(r'\[quote.*?\](.+?)\[/quote\]', r'\g<1>', bb_code, flags=re.DOTALL)
+        cleaned = re.sub(r'\[quote.*?\](.+?)\[/quote\]', r'\g<1>', bb_code, flags=re.DOTALL | re.IGNORECASE)
         lines = cleaned.split('\n')
         lines = ['>    %s' % line.strip() for line in lines]
         markdown = markdown.replace(bb_code, '\n'.join(lines))
 
     markdown = re.sub(r'\[code.*?\](.+?)\[/code\]', r'    \g<1>\n', markdown)
 
-    bb_codes = re.findall(r'\[code.*?\].*?\[/code\]', markdown, flags=re.DOTALL)
+    bb_codes = re.findall(r'\[code.*?\].*?\[/code\]', markdown, flags=re.DOTALL | re.IGNORECASE)
     for bb_code in bb_codes:
-        cleaned = re.sub(r'\[code.*?\](.+?)\[/code\]', r'\g<1>', bb_code, flags=re.DOTALL)
+        cleaned = re.sub(r'\[code.*?\](.+?)\[/code\]', r'\g<1>', bb_code, flags=re.DOTALL | re.IGNORECASE)
         lines = cleaned.split('\n')
         lines = ['    %s' % line.rstrip() for line in lines]
         markdown = markdown.replace(bb_code, '\n'.join(lines))
 
-    bb_lists = re.findall(r'\[list\].*?\[/list\]', markdown, flags=re.DOTALL)
+    bb_lists = re.findall(r'\[list\].*?\[/list\]', markdown, flags=re.DOTALL | re.IGNORECASE)
     for bb_list in bb_lists:
         items = re.findall(r'\[\*\](.*)[\n\s]+?', bb_list)
         items = ['- %s' % item for item in items]
