ebf1615283effa001742b709d7f7b4ece0ed67cc
Ole Morten <ole.morten.haaland@nordicsemi.no>
2014-05-27 15:46:44 +0200

    Remove superfluous import

--- askbot/models/__init__.py
+++ askbot/models/__init__.py
@@ -41,7 +41,6 @@ from askbot.conf import settings as askbot_settings
 from askbot.models.question import Thread
 from askbot.skins import utils as skin_utils
 from askbot.mail import messages
-from askbot.models.attachment import Attachment
 from askbot.models.question import QuestionView, AnonymousQuestion
 from askbot.models.question import DraftQuestion
 from askbot.models.question import FavoriteQuestion
